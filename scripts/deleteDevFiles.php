5e327d9eb10e65e19fe7f8682d7ab7e815b21c94
Aloysius1010 <luis06jose@gmail.com>
2021-07-12 17:33:30 -0400

    Creación de script para eliminar archivos
    Script en PHP para eliminar archivos de desarrollo, con la finalidad de
    colocar el proyecto en modo de producción, más funcional y eficiente.

---
+++ scripts/deleteDevFiles.php
@@ -0,0 +1,30 @@
+<?php
+
+deleteDirectory('../node_modules'); // Eliminar librerias de NodeJS ya compiladas
+deleteDirectory('../resources/assets'); // Elminar archivos que utiliza NodeJS al compilar
+deleteDirectory('../documentation'); // Elminar archivos de documentación
+deleteDirectory('../.git'); // Elminar carpeta git de versionado
+
+// Eliminar archivos innecesarios para el funcionamiento de la aplicacion
+unlink('../README.md');
+unlink('../webpack.mix.js');
+unlink('../package.json');
+unlink('../package-lock.json');
+unlink('../mix-manifest.json');
+unlink('../.gitignore');
+unlink('../info.txt');
+
+
+function deleteDirectory($dir)
+{
+    if (!$dh = @opendir($dir)) return;
+    while (false !== ($current = readdir($dh))) {
+        if ($current != '.' && $current != '..') {
+            if (!@unlink($dir . '/' . $current))
+                deleteDirectory($dir . '/' . $current);
+        }
+    }
+    closedir($dh);
+    echo 'Se ha borrado el directorio ' . $dir . '<br/>';
+    @rmdir($dir);
+}
