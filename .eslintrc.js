cffec34aab3c1d4a77027fbfa2568ad8d449935a
Luis Medina <luis06jose@gmail.com>
2021-10-03 03:35:10 -0400

    Modificación de la versión de VueJS
    Cambio de la versión de VueJS, de Vue 3 a Vue 2, por razones de
    compatibilidad, considerando las demás dependencias y archivos
    necesarios. Mejora en el archivo de configuración de ESLint, mediante el
    reemplazo de la extensión del archivo por “.js”.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 'off',
+  },
+};
--- resources/src/App.vue
+++ resources/src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div id="nav" class="prueba">
-    <router-link to="/">
-      Inicio
-    </router-link> |
-    <router-link to="/vender">
-      Vender
-    </router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/vender">Vender</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
