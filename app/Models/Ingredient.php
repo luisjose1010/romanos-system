ed708a46dd8d0bfdfc6a7ad733615bd3fbb5bf95
Luis Medina <luis06jose@gmail.com>
2021-10-23 11:17:38 -0400

    Mejora en el modelo para mejorar estilos y datos
    Modificación en el modelo para cambiar el estilo de los identificadores,
    provenientes de la base de datos, a “Camel case” mediante la creación de
    un modelo base y, heredando de este, los demás modelos. Cambio en el
    modelo de productos para convertir a valores booleanos los datos
    provenientes de una columna de la base de datos.

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel as Model;
 
 class Ingredient extends Model
 {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel as Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description', 'price', 'multipleIngredients'];
+
+    protected $casts = [
+        'multiple_ingredients' => 'boolean',
+    ];
+
     public function ingredients()
     {
         return $this->hasMany(Ingredient::class);
