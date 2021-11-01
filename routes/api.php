722328aa1b96d6049ed35eb35e7f5fad2ca5622a
Luis Medina <luis06jose@gmail.com>
2021-11-01 17:58:44 -0400

    Modificación de las rutas de la API
    Cambios en las rutas de la API para que estén en plural, en vez de
    singular.

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,23 @@ use Framework\Routing\Route;
 use Framework\Routing\Routes;
 
 // Productos
-Routes::api(new Route('/product', 'Api\ProductController@get', 'GET'));
-Routes::api(new Route('/product/all', 'Api\ProductController@getAll', 'GET'));
-Routes::api(new Route('/product/ingredient', 'Api\ProductController@getIngredients', 'GET'));
-Routes::api(new Route('/product/size', 'Api\ProductController@getSizes', 'GET'));
+Routes::api(new Route('/products', 'Api\ProductController@get', 'GET'));
+Routes::api(new Route('/products/all', 'Api\ProductController@getAll', 'GET'));
+Routes::api(new Route('/products/ingredients', 'Api\ProductController@getIngredients', 'GET'));
+Routes::api(new Route('/products/sizes', 'Api\ProductController@getSizes', 'GET'));
 
-Routes::api(new Route('/product', 'Api\ProductController@post', 'POST'));
+Routes::api(new Route('/products', 'Api\ProductController@post', 'POST'));
 
 
 // Ordenes
-Routes::api(new Route('/order/all', 'Api\OrderController@getAll', 'GET'));
+Routes::api(new Route('/orders/all', 'Api\OrderController@getAll', 'GET'));
 
-Routes::api(new Route('/order/all', 'Api\OrderController@postAll', 'POST'));
+Routes::api(new Route('/orders/all', 'Api\OrderController@postAll', 'POST'));
 
 
 // Ventas
-Routes::api(new Route('/sale/all', 'Api\SaleController@getAll', 'GET'));
+Routes::api(new Route('/sales/all', 'Api\SaleController@getAll', 'GET'));
 
-Routes::api(new Route('/sale', 'Api\SaleController@post', 'POST'));
+Routes::api(new Route('/sales', 'Api\SaleController@post', 'POST'));
 
-Routes::api(new Route('/sale/{id:\d+}', 'Api\SaleController@put', 'PUT'));
+Routes::api(new Route('/sales/{id:\d+}', 'Api\SaleController@put', 'PUT'));
